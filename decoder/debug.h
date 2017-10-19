f3b7eb39b264c6e8dd0f38d3ac614876f5865946
Ayyi <tim@ayyi.org>
2017-10-19 20:20:56 +0100

    Decoder: fixed debug to respect log level

--- decoder/debug.h
+++ decoder/debug.h
@@ -1,9 +1,9 @@
 #ifndef __decoder_debug_h___
 #define __decoder_debug_h___
 
-extern int _debug_;
+extern int wf_debug;
 #define PF0 printf("%s\n", __func__)
-#define dbg(A, STR, ...) printf(STR"\n", ##__VA_ARGS__)
+#define dbg(A, STR, ...) ({if(A <= wf_debug) printf(STR"\n", ##__VA_ARGS__);})
 #define perr(A, ...) {}
 #define gwarn(A, ...) {}
 
