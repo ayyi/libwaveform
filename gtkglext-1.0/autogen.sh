f17aa20856b6f9b0c7b77422ad7a984edb766805
Ayyi <tim@ayyi.org>
2020-04-19 22:02:04 +0100

    Fix make dist

--- gtkglext-1.0/autogen.sh
+++ gtkglext-1.0/autogen.sh
@@ -5,6 +5,15 @@ libtoolize --automake
 aclocal
 autoheader -Wall
 automake --gnu --add-missing -Wall
+
+# fix dist path issues in ac_config subdirectory
+sed -i -e 's/..\/compile/compile/' Makefile.in
+sed -i -e 's/..\/config.guess/config.guess/' Makefile.in
+sed -i -e 's/..\/config.sub/config.sub/' Makefile.in
+sed -i -e 's/..\/install-sh/install-sh/' Makefile.in
+sed -i -e 's/..\/ltmain.sh/ltmain.sh/' Makefile.in
+sed -i -e 's/..\/missing/missing/' Makefile.in
+
 autoconf
 touch Makefile.in
 
